4d29f878a70a61d6304cf27fa931d6a2dcd5ecda
vujjini <vujjiniram143@gmail.com>
2025-04-07 14:51:03 -0400

    commit

--- vector_store.py
+++ vector_store.py
@@ -1,1 +1,5 @@
 # we gotta take the text and clean it up, and then add it to a FAISS vector store database
+
+
+# use regex to clear common filler shit in website articles like "sponsered", "author", etc.
+# when adding it to FAISS database, keep the tags like symbol, data, title, to make retrieval efficient, also keep a tag like "stock", "forex", "real_estate", etc
