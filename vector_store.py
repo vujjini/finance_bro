cd5083405db538d9fd4c88d8470a38d8fffdeb6f
vujjini <vujjiniram143@gmail.com>
2025-04-07 15:11:53 -0400

    commit___

--- vector_store.py
+++ vector_store.py
@@ -3,3 +3,17 @@
 
 # use regex to clear common filler shit in website articles like "sponsered", "author", etc.
 # when adding it to FAISS database, keep the tags like symbol, data, title, to make retrieval efficient, also keep a tag like "stock", "forex", "real_estate", etc
+
+
+# prompting strategy:
+# we give the LLM two things: the retrieved stuff, the prompt (system and user), market trend. in user prompt, we end with based on the research analysis using the data provided
+# provided a risk assesment level on a certain scale
+
+# store that in the asset object
+# we present that to the user
+# if the user clicks portfolio adjustment, math is done it take the risk assessments of all the asset objects, put in a summary and also the vector store
+# is given to the LLM and ask it to do the same calculation at the end.
+
+
+# also add a feature for recommendations: for hackathon, lets just do forex recommendations -> same process: collect the news -> RAG -> get output for TOP K options
+# in the current market -> along with reasoning provided by the LLM -> also present the graph of each recommended asset from an API
